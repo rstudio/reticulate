add0bd529469c789301392def2778032b01c21c2
JJ Allaire <jj@rstudio.org>
2016-08-21 09:28:16 -0700

    initial commit

---
+++ src/rcpp_hello.cpp
@@ -0,0 +1,23 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// This is a simple function using Rcpp that creates an R list
+// containing a character vector and a numeric vector.
+//
+// Learn more about how to use Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//
+// and browse examples of code using Rcpp at:
+// 
+//   http://gallery.rcpp.org/
+//
+
+// [[Rcpp::export]]
+List rcpp_hello() {
+  CharacterVector x = CharacterVector::create("foo", "bar");
+  NumericVector y   = NumericVector::create(0.0, 1.0);
+  List z            = List::create(x, y);
+  return z;
+}

6f88f50f5fffd1d7dfacd48bb25561233926ab52
JJ Allaire <jj@rstudio.org>
2016-08-22 17:05:09 -0400

    return existing names in dollar operator

--- R/py_object.R
+++ R/py_object.R
@@ -1,15 +1,22 @@
 
-
-
 #' @export
 `$.py_object` <- function(x, name) {
-  attr <- py_object_get_attr(x, name)
-  if (py_object_is_callable(attr)) {
-    function(...) {
-      py_object_call(attr)
-    }
+
+  # if the name exists then return it
+  if (name %in% ls(x, all.names = TRUE, sorted = FALSE)) {
+    .subset2(x, name)
+
+    # otherwise dynamically dispatch to the object instance
   } else {
-    attr
+
+    attr <- py_object_get_attr(x, name)
+    if (py_object_is_callable(attr)) {
+      function(...) {
+        py_object_call(attr)
+      }
+    } else {
+      attr
+    }
   }
 }
 

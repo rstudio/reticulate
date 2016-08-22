9dfc96dd7be7293a0661638d49a685fecd4df4b8
JJ Allaire <jj@rstudio.org>
2016-08-22 17:15:52 -0400

    don't .subset2

--- R/py_object.R
+++ R/py_object.R
@@ -1,22 +1,13 @@
 
 #' @export
 `$.py_object` <- function(x, name) {
-
-  # if the name exists then return it
-  if (name %in% ls(x, all.names = TRUE, sorted = FALSE)) {
-    .subset2(x, name)
-
-    # otherwise dynamically dispatch to the object instance
-  } else {
-
-    attr <- py_object_get_attr(x, name)
-    if (py_object_is_callable(attr)) {
-      function(...) {
-        py_object_call(attr)
-      }
-    } else {
-      attr
+  attr <- py_object_get_attr(x, name)
+  if (py_object_is_callable(attr)) {
+    function(...) {
+      py_object_call(attr)
     }
+  } else {
+    attr
   }
 }
 

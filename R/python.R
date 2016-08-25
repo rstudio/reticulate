00bafc5cbf5e7ccb50c4a41afc1702cd8febebdf
JJ Allaire <jj@rstudio.org>
2016-08-25 09:17:19 -0400

    get docs in .DollarNames

--- R/python.R
+++ R/python.R
@@ -41,8 +41,21 @@ print.py_object <- function(x, ...) {
 #' @importFrom utils .DollarNames
 #' @export
 .DollarNames.py_object <- function(x, pattern = "") {
+
+  # get the names and filter out internal attributes (_*)
   names <- py_list_attributes(x)
   names <- names[substr(names, 1, 1) != '_']
+
+  # get the types
   attr(names, "types") <- py_get_attribute_types(x, names)
+
+  # get the doc strings
+  inspect <- py_import("inspect")
+  getdoc <- py_get_attr(inspect, "getdoc")
+  attr(names, "docs") <- sapply(names, function(name) {
+    py_call(getdoc, list(py_get_attr(x, name)))
+  })
+
+  # return
   names
 }

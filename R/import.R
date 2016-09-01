a8af678d5e738e97857cf6ded93de96f41717b74
JJ Allaire <jj@rstudio.org>
2016-09-01 14:09:32 -0400

    move tf_import function

--- R/import.R
+++
@@ -1,6 +0,0 @@
-
-tf_import <- function(module = "tensorflow") {
-  if (substring(module, 1, 10) != "tensorflow")
-    module <- paste("tensorflow", module, sep=".")
-  py_import(module)
-}
--- R/python.R
+++ R/python.R
@@ -58,3 +58,10 @@ print.py_object <- function(x, ...) {
   # return
   names
 }
+
+# conveniene function for importing tensorflow
+tf_import <- function(module = "tensorflow") {
+  if (substring(module, 1, 10) != "tensorflow")
+    module <- paste("tensorflow", module, sep=".")
+  py_import(module)
+}

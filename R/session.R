75534f551b0c564b3595d1ced4669be958d57ddf
JJ Allaire <jj@rstudio.org>
2016-09-01 15:13:46 -0400

    docs and finalization for session

--- R/session.R
+++ R/session.R
@@ -1,8 +1,30 @@
 
+#' Create a new TensorFlow session
+#'
+#' A Session object encapsulates the environment in which Operation objects are
+#' executed, and Tensor objects are evaluated.
+#'
+#' @param target Optional. The execution engine to connect to. Defaults to using
+#'   an in-process engine.
+#' @param graph: Optional. The Graph to be launched (described below).
+#' @param config (Optional.) A list with configuration options for the session.
+#'
+#' @details
+#'
+#' If no \code{graph} argument is specified when constructing the session, the
+#' default graph will be launched in the session. If you are using more than one
+#' graph (created with \code{graph} in the same process, you will have to use
+#' different sessions for each graph, but each graph can be used in multiple
+#' sessions. In this case, it is often clearer to pass the graph to be launched
+#' explicitly to the session function
+#'
+#'
 #' @export
 session <- function(target = "", graph = NULL, config = NULL) {
   tf <- tf_import()
-  tf$Session(target, graph, config)
+  sess <- tf$Session(target, graph, config)
+  reg.finalizer(sess, function(sess) { sess$close() }, onexit = TRUE)
+  sess
 }
 
 

6809f8d09dbf039508c6e777e18105b84d323e26
JJ Allaire <jj@rstudio.org>
2016-09-02 16:46:43 -0400

    add introduction to example tests

--- tests/testthat/test-examples.R
+++ tests/testthat/test-examples.R
@@ -3,12 +3,14 @@ context("examples")
 # some helpers
 run_example <- function(example) {
   env <- new.env()
-  source(system.file("examples", example, package = "tensorflow"), local = env)
+  capture.output({
+    source(system.file("examples", example, package = "tensorflow"), local = env)
+  }, type = "output")
   rm(list = ls(env), envir = env)
   gc()
 }
 
-examples <- c("hello.R")
+examples <- c("hello.R", "introduction.R")
 
 for (example in examples) {
   test_that(paste(example, "example runs successfully"), {

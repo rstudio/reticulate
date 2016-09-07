550e61e847526b55f35a2c93f1c78efafe60dd12
JJ Allaire <jj@rstudio.org>
2016-09-07 13:37:34 -0400

    more cleanup of names

--- R/tensorflow.R
+++ R/tensorflow.R
@@ -1,4 +1,17 @@
 
+#' @useDynLib tensorflow
+#' @importFrom Rcpp evalCpp
+NULL
+
+.onLoad <- function(libname, pkgname) {
+  py_initialize(pythonSharedLibrary());
+}
+
+.onUnload <- function(libpath) {
+  py_finalize();
+}
+
+
 # convenience function for importing tensorflow
 #' @export
 tensorflow <- function(module = NULL) {

a3b122afa22de3cb95169eae91ba4d2650042975
JJ Allaire <jj@rstudio.org>
2016-09-07 14:30:52 -0400

    more math generics

--- R/tensorflow.R
+++ R/tensorflow.R
@@ -21,28 +21,6 @@ tensorflow <- function(module = NULL) {
     py_import(paste("tensorflow", module, sep="."))
 }
 
-# Some math generics, see here for docs on more generics:
-# https://stat.ethz.ch/R-manual/R-devel/library/base/html/groupGeneric.html
-
-# __neg__ (unary -)
-# __abs__ (abs())
-# __invert__ (unary ~)
-# __add__ (binary +)
-# __sub__ (binary -)
-# __mul__ (binary elementwise *)
-# __div__ (binary / in Python 2)
-# __floordiv__ (binary // in Python 3)
-# __truediv__ (binary / in Python 3)
-# __mod__ (binary %)
-# __pow__ (binary **)
-# __and__ (binary &)
-# __or__ (binary |)
-# __xor__ (binary ^)
-# __lt__ (binary <)
-# __le__ (binary <=)
-# __gt__ (binary >)
-# __ge__ (binary >=)
-
 #' @export
 "+.tensorflow.python.framework.ops.Tensor" <- function(a, b) {
   tensorflow()$add(a, b)
@@ -85,12 +63,37 @@ tensorflow <- function(module = NULL) {
 }
 
 #' @export
+"/.tensorflow.python.framework.ops.Variable" <- function(a, b) {
+  tensorflow()$truediv(a, b)
+}
+
+#' @export
 "%/%.tensorflow.python.framework.ops.Tensor" <- function(a, b) {
+  tensorflow()$floordiv(a, b)
+}
+
+#' @export
+"%/%.tensorflow.python.framework.ops.Variable" <- function(a, b) {
   tensorflow()$floordiv(a, b)
 }
 
 #' @export
 "%%.tensorflow.python.framework.ops.Tensor" <- function(a, b) {
   tensorflow()$mod(a, b)
+}
+
+#' @export
+"%%.tensorflow.python.framework.ops.Variable" <- function(a, b) {
+  tensorflow()$mod(a, b)
+}
+
+#' @export
+"^.tensorflow.python.framework.ops.Tensor" <- function(a, b) {
+  tensorflow()$pow(a, b)
+}
+
+#' @export
+"^.tensorflow.python.framework.ops.Variable" <- function(a, b) {
+  tensorflow()$pow(a, b)
 }
 

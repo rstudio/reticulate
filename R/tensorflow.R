43b97db65e7915a1b1989aae8acd9686d00d1651
JJ Allaire <jj@rstudio.org>
2016-09-08 16:38:05 -0400

    some additional math S3 generics

--- R/tensorflow.R
+++ R/tensorflow.R
@@ -269,5 +269,172 @@ shape <- function(...) {
   tf$greater_equal(a, b)
 }
 
+#' @export
+"abs.tensorflow.python.framework.ops.Tensor" <- function(x) {
+  tf$abs(x)
+}
+
+#' @export
+"abs.tensorflow.python.framework.ops.Variable" <- function(x) {
+  tf$abs(x)
+}
+
+#' @export
+"sign.tensorflow.python.framework.ops.Tensor" <- function(x) {
+  tf$sign(x)
+}
+
+#' @export
+"sign.tensorflow.python.framework.ops.Variable" <- function(x) {
+  tf$sign(x)
+}
+
+#' @export
+"sqrt.tensorflow.python.framework.ops.Tensor" <- function(x) {
+  tf$sqrt(x)
+}
+
+#' @export
+"sqrt.tensorflow.python.framework.ops.Variable" <- function(x) {
+  tf$sqrt(x)
+}
+
+#' @export
+"floor.tensorflow.python.framework.ops.Tensor" <- function(x) {
+  tf$floor(x)
+}
+
+#' @export
+"floor.tensorflow.python.framework.ops.Variable" <- function(x) {
+  tf$floor(x)
+}
+
+#' @export
+"ceiling.tensorflow.python.framework.ops.Tensor" <- function(x) {
+  tf$ceil(x)
+}
+
+#' @export
+"ceiling.tensorflow.python.framework.ops.Variable" <- function(x) {
+  tf$ceil(x)
+}
+
+#' @export
+"round.tensorflow.python.framework.ops.Tensor" <- function(x, digits = 0) {
+  if (digits != 0)
+    stop("TensorFlow round only supports rounding to integers")
+  tf$round(x)
+}
+
+#' @export
+"round.tensorflow.python.framework.ops.Variable" <- function(x, digits = 0) {
+  if (digits != 0)
+    stop("TensorFlow round only supports rounding to integers")
+  tf$round(x)
+}
+
+#' @export
+"exp.tensorflow.python.framework.ops.Tensor" <- function(x) {
+  tf$exp(x)
+}
+
+#' @export
+"exp.tensorflow.python.framework.ops.Variable" <- function(x) {
+  tf$exp(x)
+}
+
+#' @export
+"log.tensorflow.python.framework.ops.Tensor" <- function(x, base = exp(1)) {
+  if (base != exp(1))
+    stop("TensorFlow log suppports only natural logarithms")
+  tf$log(x)
+}
+
+#' @export
+"log.tensorflow.python.framework.ops.Variable" <- function(x, base = exp(1)) {
+  if (base != exp(1))
+    stop("TensorFlow log suppports only natural logarithms")
+  tf$log(x)
+}
+
+#' @export
+"cos.tensorflow.python.framework.ops.Tensor" <- function(x) {
+  tf$cos(x)
+}
+
+#' @export
+"cos.tensorflow.python.framework.ops.Variable" <- function(x) {
+  tf$cos(x)
+}
+
+#' @export
+"sin.tensorflow.python.framework.ops.Tensor" <- function(x) {
+  tf$sin(x)
+}
+
+#' @export
+"sin.tensorflow.python.framework.ops.Variable" <- function(x) {
+  tf$sin(x)
+}
+
+#' @export
+"tan.tensorflow.python.framework.ops.Tensor" <- function(x) {
+  tf$tan(x)
+}
+
+#' @export
+"tan.tensorflow.python.framework.ops.Variable" <- function(x) {
+  tf$tan(x)
+}
+
+#' @export
+"acos.tensorflow.python.framework.ops.Tensor" <- function(x) {
+  tf$acos(x)
+}
+
+#' @export
+"acos.tensorflow.python.framework.ops.Variable" <- function(x) {
+  tf$acos(x)
+}
+
+#' @export
+"asin.tensorflow.python.framework.ops.Tensor" <- function(x) {
+  tf$asin(x)
+}
+
+#' @export
+"asin.tensorflow.python.framework.ops.Variable" <- function(x) {
+  tf$asin(x)
+}
+
+#' @export
+"atan.tensorflow.python.framework.ops.Tensor" <- function(x) {
+  tf$atan(x)
+}
+
+#' @export
+"atan.tensorflow.python.framework.ops.Variable" <- function(x) {
+  tf$atan(x)
+}
+
+#' @export
+"lgamma.tensorflow.python.framework.ops.Tensor" <- function(x) {
+  tf$lgamma(x)
+}
+
+#' @export
+"lgamma.tensorflow.python.framework.ops.Variable" <- function(x) {
+  tf$lgamma(x)
+}
+
+#' @export
+"digamma.tensorflow.python.framework.ops.Tensor" <- function(x) {
+  tf$digamma(x)
+}
+
+#' @export
+"digamma.tensorflow.python.framework.ops.Variable" <- function(x) {
+  tf$digamma(x)
+}
 
 

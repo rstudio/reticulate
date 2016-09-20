016ab11bc5d9788a9fed6ab2e25c82e2e5344ea1
JJ Allaire <jj@rstudio.org>
2016-09-20 11:39:19 -0400

    include all examples in tests and provide option to control testing of examples

--- tests/testthat/test-examples.R
+++ tests/testthat/test-examples.R
@@ -6,13 +6,23 @@ source("utils.R")
 run_example <- function(example) {
   env <- new.env()
   capture.output({
-    source(system.file("examples", example, package = "tensorflow"), local = env)
+    example_path <- system.file("examples", example, package = "tensorflow")
+    old_wd <- setwd(dirname(example_path))
+    on.exit(setwd(old_wd), add = TRUE)
+    source(basename(example_path), local = env)
   }, type = "output")
   rm(list = ls(env), envir = env)
   gc()
 }
 
-examples <- c("hello.R", "introduction.R", "mnist/mnist_softmax.R")
+if (nzchar(Sys.getenv("TENSORFLOW_TEST_EXAMPLES"))) {
+  examples <- c("hello.R",
+                "introduction.R",
+                "mnist/mnist_softmax.R",
+                "mnist/fully_connected_feed.R")
+} else {
+  examples <- c()
+}
 
 for (example in examples) {
   test_that(paste(example, "example runs successfully"), {

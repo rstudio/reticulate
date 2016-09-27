8e1130a5a0ff8fc0e879f6dec755f59338f5b225
JJ Allaire <jj@rstudio.org>
2016-09-27 15:45:26 -0400

    move docs

--- R/tf_docs.R
+++
@@ -1,6 +0,0 @@
-
-
-view_docs <- function() {
-  open <- ifelse(Sys.info()[["sysname"]] == "Darwin", "open", "xdg-open")
-  system(paste(open, system.file("docs/index.html", package = "tensorflow")))
-}

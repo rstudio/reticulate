33b163e643537fe056322d917dc792dadac9c590
JJ Allaire <jj@rstudio.org>
2016-09-27 15:57:19 -0400

    package documentation page

--- R/package.R
+++ R/package.R
@@ -1,7 +1,25 @@
 
 
-
-
+#' TensorFlow for R
+#'
+#' \href{https://tensorflow.org}{TensorFlow} is an open source software library
+#' for numerical computation using data flow graphs. Nodes in the graph
+#' represent mathematical operations, while the graph edges represent the
+#' multidimensional data arrays (tensors) communicated between them. The
+#' flexible architecture allows you to deploy computation to one or more CPUs or
+#' GPUs in a desktop, server, or mobile device with a single API.
+#'
+#' The \href{https://www.tensorflow.org/api_docs/python/index.html}{TensorFlow
+#' API} is composed of a set of Python modules that enable constructing and
+#' executing TensorFlow graphs. The tensorflow package provides access to the
+#' complete TensorFlow API from within R.
+#'
+#' For additional documentation on the tensorflow package see
+#' \href{https://rstudio.github.io/tensorflow}{https://rstudio.github.io/tensorflow}
+#'
+#'
+#' @docType package
+#' @name tensorflow
 #' @useDynLib tensorflow
 #' @importFrom Rcpp evalCpp
 NULL

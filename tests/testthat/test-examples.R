493c2c31ed0aaffd0bb54785befab39d01f028cb
Terry Tang <terrytangyuan@gmail.com>
2016-11-29 11:49:25 -0600

    Added new example to test

--- tests/testthat/test-examples.R
+++ tests/testthat/test-examples.R
@@ -19,7 +19,8 @@ if (nzchar(Sys.getenv("TENSORFLOW_TEST_EXAMPLES"))) {
   examples <- c("hello.R",
                 "introduction.R",
                 "mnist/mnist_softmax.R",
-                "mnist/fully_connected_feed.R")
+                "mnist/fully_connected_feed.R",
+                "tflearn/iris_dnn.R")
 } else {
   examples <- c()
 }

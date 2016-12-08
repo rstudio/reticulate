a1bed771f0a38c48e8c6efdede574a6002300b15
JJ Allaire <jj@rstudio.org>
2016-12-07 21:05:02 -0500

    add test for iterator draining behavior

--- tests/testthat/test-python-iterators.R
+++ tests/testthat/test-python-iterators.R
@@ -12,3 +12,11 @@ test_that("Iterators reflect values back", {
 test_that("Generators reflect values back", {
   expect_equal(as.integer(iterate(test$makeGenerator(5))) + 1L, seq(5))
 })
+
+
+test_that("Iterators are drained of their values by iteration", {
+  iter <- test$makeIterator(c(1:5))
+  a <- iterate(iter)
+  b <- iterate(iter)
+  expect_length(b, 0)
+})

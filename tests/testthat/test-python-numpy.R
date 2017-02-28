786358785b76e83cffa7a0d2097b83db8554771f
JJ Allaire <jj@rstudio.org>
2017-02-28 15:30:54 -0500

    improved check for numpy test environment

--- tests/testthat/test-python-numpy.R
+++ tests/testthat/test-python-numpy.R
@@ -4,6 +4,7 @@ source("utils.R")
 
 test_that("R matrixes are converted to numpy ndarray", {
   skip_if_no_numpy()
+  np <- import("numpy")
   m1 <- matrix(c(1,2,3,4), nrow = 2, ncol = 2)
   m2 <- m1
   expect_equal(np$equal(m1,m2), m1 == m2)
@@ -11,18 +12,21 @@ test_that("R matrixes are converted to numpy ndarray", {
 
 test_that("Numpy ndarray is converted to R matrix", {
   skip_if_no_numpy()
+  np <- import("numpy")
   m1 <- np$matrix(list(c(1,2), c(3,4)))
   expect_equal(m1, matrix(c(1,2,3,4), nrow = 2, ncol = 2, byrow = TRUE))
 })
 
 test_that("Multi-dimensional arrays are handled correctly", {
   skip_if_no_numpy()
+  np <- import("numpy")
   a1 <- array(c(1:8), dim = c(2,2,2))
   expect_equal(-a1, np$negative(a1))
 })
 
 test_that("Character arrays are handled correctly", {
   skip_if_no_numpy()
+  np <- import("numpy")
   a1 <- array(as.character(c(1:8)), dim = c(2,2,2))
   expect_equal(a1, py_to_r(r_to_py(a1)))
 })

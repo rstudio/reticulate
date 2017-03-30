941ea42d1eec607d31db035f1bb4a43d72a77881
terrytangyuan <terrytangyuan@gmail.com>
2017-03-30 17:50:30 -0500

    Added comparison of generated wrapper in test

--- tests/testthat/test_py_function_wrapper.R
+++ tests/testthat/test_py_function_wrapper.R
@@ -5,4 +5,7 @@ source("utils.R")
 test_that("test_py_function_wrapper() generates wrapper correctly", {
   skip_if_no_python()
   generated_wrapper <- py_function_wrapper("test$test_py_function_wrapper")
+  generated_output <- capture.output(generated_wrapper)
+  expected_output <- readLines("expected_wrapper.txt")
+  expect_equal(generated_output, expected_output)
 })

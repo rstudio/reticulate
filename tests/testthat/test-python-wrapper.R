78ec418a9885d5865ba3d8d1177680ff22538859
terrytangyuan <terrytangyuan@gmail.com>
2017-04-01 15:14:06 -0500

    Added test case for class wrapper

--- tests/testthat/test-python-wrapper.R
+++ tests/testthat/test-python-wrapper.R
@@ -2,10 +2,19 @@ context("wrapper")
 
 source("utils.R")
 
-test_that("test_py_function_wrapper() generates wrapper correctly", {
+test_that("test_py_function_wrapper() generates wrapper correctly for functions", {
   skip_if_no_python()
   generated_wrapper <- py_function_wrapper("test$test_py_function_wrapper")
   generated_output <- capture.output(generated_wrapper)
-  expected_output <- readLines("expected_wrapper.txt")
+  expected_output <- readLines("expected_function_wrapper.txt")
   expect_equal(generated_output, expected_output)
 })
+
+test_that("test_py_function_wrapper() generates wrapper correctly for clases", {
+  skip_if_no_python()
+  generated_wrapper <- py_function_wrapper("test$TestPyFunctionWrapperClass")
+  generated_output <- capture.output(generated_wrapper)
+  expected_output <- readLines("expected_class_wrapper.txt")
+  expect_equal(generated_output, expected_output)
+})
+

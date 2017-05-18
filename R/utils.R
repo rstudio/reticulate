686d4acdd84bf106872ff1b248c4ca4631dc089d
JJ Allaire <jj@rstudio.com>
2017-05-18 14:10:57 -0400

    more robust check for no retvalue in error handler

--- R/utils.R
+++ R/utils.R
@@ -31,10 +31,10 @@ traceback_enabled <- function() {
 clear_error_handler <- function(retvalue = NA) {
   function(e) {
     py_clear_last_error()
-    if (!is.na(retvalue))
-      retvalue
-    else
+    if (!is.null(retvalue) && is.na(retvalue))
       e
+    else
+      retvalue
   }
 }
 

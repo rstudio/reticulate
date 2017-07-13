e9d6018f95380c2db5b34f73c09031f89c11be46
jjallaire <jj@rstudio.com>
2017-07-13 15:19:58 -0400

    null check for config$libpython

--- R/package.R
+++ R/package.R
@@ -63,7 +63,7 @@ initialize_python <- function(required_module = NULL) {
   # check for basic python prerequsities
   if (is.null(config)) {
     stop("Installation of Python not found, Python bindings not loaded.")
-  } else if (!is_windows() && !file.exists(config$libpython)) {
+  } else if (!is_windows() && (is.null(config$libpython) || !file.exists(config$libpython))) {
     stop("Python shared library '", config$libpython, "' not found, Python bindings not loaded.")
   } else if (is_incompatible_arch(config)) {
     stop("Your current architecture is ", current_python_arch(), " however this version of ",

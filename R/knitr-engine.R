b5c9d9b114bc287a732204133ff34232c62c52e5
Kevin Ushey <kevinushey@gmail.com>
2017-10-09 11:34:38 -0700

    use AST-powered approach

--- R/knitr-engine.R
+++ R/knitr-engine.R
@@ -20,64 +20,57 @@
 #' @export
 eng_python <- function(options) {
   
+  ast <- import("ast")
+  
+  # helper function for extracting range of code, dropping blank lines
+  extract <- function(code, range) {
+    snippet <- code[range[1]:range[2]]
+    snippet[nzchar(snippet)]
+  }
+  
   # extract the code to be run -- we'll attempt to run the code line by line
   # and detect changes so that we can interleave code and output (similar to
   # what one sees when executing an R chunk in knitr). to wit, we'll do our
   # best to emulate the return format of 'evaluate::evaluate()'
   code <- options$code
-  if (!length(code))
+  n <- length(code)
+  if (n == 0)
     return(list())
   
-  # helper function for extracting code
-  extract <- function(code, start, end) {
-    pasted <- paste(code[start:end], collapse = "\n")
-    sub("^[\\r\\n\\t ]*\\n", "", pasted)
-  }
+  # use 'ast.parse()' to parse Python code and collect line numbers, so we
+  # can split source code into statements
+  pasted <- paste(code, collapse = "\n")
+  parsed <- ast$parse(pasted, "<string>")
   
-  # indices tracking the current lines of code to be executed
-  start <- end <- 1
-  n <- length(code)
+  # iterate over top-level nodes and extract line numbers
+  lines <- vapply(parsed$body, function(node) {
+    node$lineno
+  }, integer(1))
   
-  # record where pending console output should be collected from
+  # convert from lines to ranges
+  starts <- lines
+  ends <- c(lines[-1] - 1, length(code))
+  ranges <- mapply(c, starts, ends, SIMPLIFY = FALSE)
+  
+  # line index from which source should be emitted
   pending <- 1
   
   # actual outputs to be returned to knitr
   outputs <- list()
   
-  while (TRUE) {
+  for (range in ranges) {
     
-    # check for end situation
-    if (start > n || end > n)
-      break
-    
-    # attempt evaluate of current chunk of code
-    snippet <- extract(code, start, end)
+    # evaluate current chunk of code
+    snippet <- extract(code, range)
     captured <- py_capture_output(
-      result <- tryCatch(
-        py_run_string(snippet, convert = FALSE),
-        error = identity
-      )
+      py_run_string(snippet, convert = FALSE)
     )
     
-    # detect syntax errors: assume that this means we need to pass an extra
-    # line of input to reticulate
-    #
-    # TODO: it'd be nice if we could submit incomplete lines of code to
-    # reticulate
-    if (inherits(result, "error")) {
-      message <- result$message
-      if (is.character(message) && any(grepl("SyntaxError", message))) {
-        end <- end + 1
-        next
-      }
-    }
-    
-    # if we have console output, append that
     if (is.character(captured) && nzchar(captured)) {
       
       # append pending source to outputs
       outputs[[length(outputs) + 1]] <- structure(
-        list(src = extract(code, pending, end)),
+        list(src = extract(code, c(pending, range[2]))),
         class = "source"
       )
       
@@ -86,24 +79,23 @@ eng_python <- function(options) {
       
       # TODO: find and append image data?
       
-      # update pending state
-      pending <- end + 1
+      # update pending source range
+      pending <- range[2] + 1
     }
-    
-    # bump the start and end indices and continue
-    end <- end + 1
-    start <- end
   }
   
   # if we have leftover input, add that now
   if (pending < n) {
-    leftover <- extract(code, pending, n)
+    leftover <- extract(code, c(pending, n))
     outputs[[length(outputs) + 1]] <- structure(
       list(src = leftover),
       class = "source"
     )
   }
   
+  # TODO: development version of knitr supplies new 'engine_output()'
+  # interface -- use that when it's on CRAN
+  # https://github.com/yihui/knitr/commit/71bfd8796d485ed7bb9db0920acdf02464b3df9a
   wrap <- yoink("knitr", "wrap")
   wrap(outputs, options)
   

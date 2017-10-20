a419882cc01f5c9ff8270d45559ce7047c8b13f9
Kevin Ushey <kevinushey@gmail.com>
2017-10-20 10:21:15 -0700

    tweaks for tests

--- tests/testthat/test-python-comparisons.R
+++ tests/testthat/test-python-comparisons.R
@@ -4,8 +4,9 @@ source("utils.R")
 
 test_that("Python integers can be compared", {
   skip_if_no_python()
-  a <- py$int(1)
-  b <- py$int(2)
+  builtins <- import_builtins(convert = FALSE)
+  a <- builtins$int(1)
+  b <- builtins$int(2)
   expect_true(a < b)
   expect_true(b > a)
   expect_true(a == a)
@@ -16,8 +17,9 @@ test_that("Python integers can be compared", {
 
 test_that("Python objects with the same reference are equal", {
   skip_if_no_python()
-  list_fn <- py$list
-  expect_true(list_fn == py$list)
+  builtins <- import_builtins(convert = FALSE)
+  list_fn <- builtins$list
+  expect_true(list_fn == builtins$list)
 })
 
 
--- tests/testthat/test-python-knitr-engine.R
+++ tests/testthat/test-python-knitr-engine.R
@@ -2,6 +2,15 @@ context("knitr")
 
 test_that("An R Markdown document can be rendered using reticulate", {
   skip_if_not_installed("rmarkdown")
+  
+  modules <- c("numpy", "matplotlib")
+  for (module in modules) {
+    if (!py_module_available(module)) {
+      fmt <- "module '%s' not available; skipping"
+      skip(sprintf(fmt, module))
+    }
+  }
+  
   status <- withr::with_dir("resources", {
     rmarkdown::render("example.Rmd", quiet = TRUE)
   })

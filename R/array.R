21aa85b47cbd705676dc4ad95072662428d630d5
jjallaire <jj@rstudio.com>
2017-10-25 10:14:55 -0400

    regenerate docs

--- R/array.R
+++ R/array.R
@@ -46,17 +46,17 @@ np_array <- function(data, dtype = NULL, order = "C") {
 
 #' Reshape an Array
 #' 
-#' Reshape (reindex) a multi-dimensional array, using reshaping semantics that
-#' align with the behavior of NumPy's own `reshape()` function.
+#' Reshape (reindex) a multi-dimensional array, using row-major (C-style) reshaping
+#' semantics by default.
 #' 
 #' This function differs from e.g. `dim(x) <- dim` in a very important way: by
 #' default, `array_reshape()` will fill the new dimensions in row-major (`C`-style)
 #' ordering, while [dim<-()] will fill new dimensions in column-major
-#' (`F`ortran-style) ordering. This is done to be consistent with the NumPy
-#' `np.reshape()` function, which defaults to this sort of ordering when
+#' (`F`ortran-style) ordering. This is done to be consistent with libraries
+#' like NumPy, Keras, and TensorFlow, which default to this sort of ordering when
 #' reshaping arrays. See the examples for why this difference may be important.
 #' 
-#' @param x Either an \R array or a NumPy array.
+#' @param x An array
 #' @param dim The new dimensions to be set on the array.
 #' @param order The order in which elements of `x` should be read during
 #'   the rearrangement. `"C"` means elements should be read in row-major

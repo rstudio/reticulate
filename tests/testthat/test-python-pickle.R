749a2f2446313de4b07592091598d5df2469df32
JJ Allaire <jj@rstudio.com>
2017-11-16 07:43:06 -0500

    remove cPickle test

--- tests/testthat/test-python-pickle.R
+++ tests/testthat/test-python-pickle.R
@@ -13,14 +13,3 @@ test_that("Objects can be saved and loaded with pickle", {
   expect_true(x == y)
 })
 
-
-test_that("The cPickle implementation can be used", {
-  skip_if_no_python()
-  expect_error({
-    x <- dict()
-    x$a <- 1
-    x$b <- 2
-    py_save_object(x, "x.pickle", pickle = "cPickle")
-    unlink("x.pickle")
-  }, NA)
-})

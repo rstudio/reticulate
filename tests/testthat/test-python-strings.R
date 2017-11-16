b78bcd74fa0fd8610a807bb37e2acdd01442102f
JJ Allaire <jj@rstudio.com>
2017-11-16 08:12:03 -0500

    skip unicode string test on windows (can't pass string literal in py_run_string)

--- tests/testthat/test-python-strings.R
+++ tests/testthat/test-python-strings.R
@@ -4,6 +4,7 @@ source("utils.R")
 
 test_that("Unicode strings are handled by py_str", {
   skip_if_no_python()
+  skip_on_os("windows")
   main <- py_run_string("x = u'\xfc'", convert = FALSE)
   expect_equal(py_str(main$x), "ü")
 })

85ddbafb1cbadf58dff7e184a0e5698e3240ae11
jjallaire <jj@rstudio.com>
2017-11-27 08:50:44 -0500

    skip delay_load test on travis

--- tests/testthat/test-delay-load.R
+++ tests/testthat/test-delay-load.R
@@ -6,13 +6,14 @@ library(callr)
 
 test_that("imoprted module can be customized via delay_load", {
   skip_if_no_python()
+  skip_on_travis()
   expect_true(r(function() {
     sys <- reticulate::import("invalid_module_name", delay_load = list(
       get_module = function() {
         "sys"
       }
     ))
-    is.character(sys$copyright)
+    is.character(sys$byteorder)
   }))
 })
 

9c344f30548defb7def825224f802b2cfe0effbc
terrytangyuan <terrytangyuan@gmail.com>
2018-04-15 22:26:23 -0400

    Add a function for custom scaffold R wrappers

--- R/wrapper.R
+++ R/wrapper.R
@@ -130,7 +130,146 @@ print.py_wrapper <- function(x, ...) {
 }
 
 
-
+#' Custom Scaffolding of R Wrappers for Python Functions
+#'
+#' This utility function can be used to generate R wrapper for a specified
+#' Python function while allowing to inject custom code for critical parts of
+#' the wrapper generation, such as process the any part of the docs obtained
+#' from [py_function_docs()] and append additional roxygen fields. The result
+#' from execution of `python_function` is assigned to a variable called
+#' `python_function_result` that can also be processed by `postprocess_fn`
+#' before writing the closing curly braces for the generated wrapper function.
+#'
+#' @inheritParams py_function_wrapper
+#' @param additional_roxygen_fields A list of additional roxygen fields to write
+#'   to the roxygen docs, e.g. `list(export = "", rdname =
+#'   "generated-wrappers")`.
+#'
+#' @param process_docs_fn A function to process docs obtained from
+#'   `reticulate::py_function_docs(python_function)`.
+#' @param process_param_fn A function to process each parameter needed for
+#'   `python_funcion` before executing `python_funcion.`
+#' @param process_param_doc_fn A function to process the roxygen docstring for
+#'   each parameter.
+#' @param postprocess_fn A function to inject any custom code in the form of a
+#'   string before writing the closing curly braces for the generated wrapper
+#'   function.
+#' @param file_name The file name to write the generated wrapper function to. If
+#'   `NULL`, the generated wrapper will only be printed out in the console.
+#'
+#' @examples
+#' \dontrun{
+#' 
+#' library(tensorflow)
+#' library(stringr)
+#'
+#' # Example of a `process_param_fn` to cast parameters with default values that contains "L" to integers
+#' process_int_param_fn <- function(param, docs) {
+#'   # Extract the params with integer types
+#'   int_params <- gsub(
+#'     " = [-]?[0-9]+L",
+#'     "",
+#'     str_extract_all(docs$signature, "[A-z]+ = [-]?[0-9]+L")[[1]])
+#'   # Custom handling of integer params
+#'   if (param %in% int_params) {
+#'     param <- paste0("as.integer(", param, ")")
+#'   }
+#'   param
+#' }
+#' 
+#' # Note that since the default value of parameter `k` is `1L`. It is wrapped
+#' # by `as.integer()` to ensure it's casted to integer before sending it to `tf$nn$top_k` 
+#' # for execution. We then print out the python function result.
+#' 
+#' py_function_custom_scaffold(
+#'   "tf$nn$top_k",
+#'   r_function = "top_k",
+#'   process_param_fn = process_int_param_fn,
+#'   postprocess_fn = function() { "print(python_function_result)" })
+#'
+#' }
+py_function_custom_scaffold <- function(
+  python_function,
+  r_function = NULL,
+  additional_roxygen_fields = NULL,
+  process_docs_fn = function(docs) docs,
+  process_param_fn = function(param, docs) param,
+  process_param_doc_fn = function(param_doc, docs) param_doc,
+  postprocess_fn = function() {},
+  file_name = NULL
+) {
+  
+  write_line <- function(text) {
+    cat(text, sep = "\n")
+  }
+  
+  wrapper_output <- capture.output(
+    {
+      docs <- reticulate::py_function_docs(python_function)
+      docs <- process_docs_fn(docs)
+      
+      write_line(paste0("#' @description ", docs$description))
+      write_line(paste0("#' @title ", docs$name))
+      write_line("#' ")
+      
+      # Write docstrings for each parameters
+      for(i in 1:length(docs$parameters)) {
+        param_doc <- process_param_doc_fn(docs$sections[i], docs)
+        write_line(paste0("#' @param ", " ", names(docs$parameters)[i], " ", param_doc))
+      }
+      
+      # Write additional roxygen fields
+      if (!is.null(additional_roxygen_fields)) {
+        if (!is.list(additional_roxygen_fields))
+          stop("additional_roxygen_fields_list must be a list")
+        invisible(lapply(names(additional_roxygen_fields), function(field_name) {
+          write_line(paste0("#' @", field_name, " ", additional_roxygen_fields[[field_name]]))
+        }))
+      }
+      
+      # Change the name of the wrapper
+      if (is.null(r_function)) {
+        r_function <- docs$name
+      }
+      
+      # Generate function signature
+      signature <- sub(paste0(docs$name, "\\("),
+                       paste(r_function, "<- function(\n\t"), docs$signature)
+      write_line(paste(signature, "{\n"))
+      
+      # Execute the Python function
+      write_line(paste0("  python_function_result <- ", python_function, "("))
+      
+      # Generate parameters that get passed to the Python function call
+      params <- names(docs$parameters)
+      if (length(params) > 0) {
+        for (i in 1:length(params)) {
+          param <- params[i]
+          suffix <- ifelse(i < length(params), ",", "\n  )")
+          write_line(
+            paste0(
+              "    ",
+              params[[i]], " = ",
+              process_param_fn(param, docs),
+              suffix))
+        }
+      } else {
+        write_line(")")
+      }
+      
+      # Inject additional custom code
+      write_line(paste0("  ", postprocess_fn()))
+      
+      write_line("}\n")
+    }
+  )
+  
+  if (is.null(file_name)) {
+    cat(wrapper_output, sep = "\n")
+  } else {
+    write(wrapper_output, file = file_name, append = TRUE)
+  }
+}
 
 
 

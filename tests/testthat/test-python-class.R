eb4e09c2520654ed42585c8de1f12f57f3fc6993
Daniel Falbel <dfalbel@gmail.com>
2020-01-06 14:49:25 -0300

    third level of inheritance that causes an infinite recursion

--- tests/testthat/test-python-class.R
+++ tests/testthat/test-python-class.R
@@ -214,7 +214,32 @@ test_that("self is not converted when there's a py_to_r method for it", {
   rm(py_to_r.python.builtin.Base, envir = .GlobalEnv)
 })
 
-
+test_that("can call super from an ingerited class", {
+  
+  Base1 <- PyClass("Base1", list(`__init__` = function(self, a) {
+    self$a <- a
+  }))
+  
+  Base2 <- PyClass("Base2", list(`__init__` = function(self, a, b) {
+    self$b <- b
+    super()$`__init__`(a)
+  }), inherit = Base1)
+  
+  Inhe <- PyClass("Inhe", inherit = Base2, list(
+    `__init__` = function(self, a, b, c) {
+      self$c <- c
+      super()$`__init__`(a, b)
+      NULL
+    }
+  ))
+  
+  x <- Inhe(10, 20, 30)
+  
+  
+  expect_equal(x$a, 10)
+  expect_equal(x$b, 20)
+  expect_equal(x$c, 30)
+})
 
 
 

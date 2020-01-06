2624384781a0314394e54ac5b4f6972eb4fbbc31
Daniel Falbel <dfalbel@gmail.com>
2020-01-06 14:50:11 -0300

    adds a reference to the created class so multiple levels of inheritance works correctly

--- R/class.R
+++ R/class.R
@@ -2,19 +2,35 @@ inject_super <- function(fun) {
   # for each function `fun` we need to place a `super` function in its
   # search path, and this `super` function must be able to access
   # the `self` argument passed to `fun`.
-  
+
   e <- new.env(parent = environment(fun))
   
   e$super <- function() {
     bt <- reticulate::import_builtins()
     # self is an argument passed to fun
-    self <- get("self", envir = parent.frame(), inherits = FALSE) 
-    bt$super(self$`__class__`, self)
+    self <- get("self", envir = parent.frame(), inherits = FALSE)
+    class_ <- get("__class__", envir = e, inherits = FALSE)
+    bt$super(class_, self)
   }
-  
+
   environment(fun) <- e # so fun can access `super`
-  
+
   fun
+}
+
+#' Enable the convertion scope so `self` fields can be accessed
+#' without the need to call `py_to_r`.
+#' 
+#' @param f a method/function of a Python class
+#' 
+enable_convert_scope <- function(f) {
+  function(...) {
+    args <- list(...)
+    # enable convertion scope for `self`
+    # the first argument is always `self`.and we don't want to convert it.
+    assign("convert", TRUE, envir = as.environment(args[[1]])) 
+    do.call(f, append(args[1], lapply(args[-1], py_to_r)))
+  }
 }
 
 #' Create a python class
@@ -56,20 +72,25 @@ PyClass <- function(classname, defs = list(), inherit = NULL) {
   defs <- lapply(defs, function(x) {
     if (inherits(x, "function")) {
       f <- inject_super(x)
-      x <- function(...) {
-        args <- list(...)
-        # enable convertion scope for `self`
-        # the first argument is always `self`.and we don't want to convert it.
-        assign("convert", TRUE, envir = as.environment(args[[1]])) 
-        do.call(f, append(args[1], lapply(args[-1], py_to_r)))
-      }
+      x <- enable_convert_scope(f)
+      attr(x, "__env__") <- environment(f)
     }
     x
   })
   
-  bt$type(
+  type <- bt$type(
     classname, inherit, 
     do.call(reticulate::dict, defs)
   )
+  
+  # we add a reference to the type here. so it can be acessed without needing
+  # to find the type from self.
+  lapply(defs, function(x) {
+    if(!is.null(e <- attr(x, "__env__"))) {
+      e$`__class__` <- type
+    }
+  })
+  
+  type
 }
 

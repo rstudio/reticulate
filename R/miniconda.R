rstudio/reticulate

commit 9cf3178d344c6ed61e9648127038c020f1228a60

support non-intel architectures for miniconda installer

R/miniconda.R
@@ -93,7 +93,9 @@ install_miniconda_preflight <- function(path, force) {
 miniconda_installer_url <- function(version = "3") {
   
   base <- "https://repo.anaconda.com/miniconda"
-  arch <- ifelse(.Machine$sizeof.pointer == 8, "x86_64", "x86")
+  
+  info <- as.list(Sys.info())
+  arch <- miniconda_installer_arch()
   version <- as.character(version)
   name <- if (is_windows())
     sprintf("Miniconda%s-latest-Windows-%s.exe", version, arch)
@@ -105,6 +107,23 @@ miniconda_installer_url <- function(version = "3") {
     stopf("unsupported platform %s", shQuote(Sys.info()[["sysname"]]))
   
   file.path(base, name)
+  
+}
+
+miniconda_installer_arch <- function() {
+  
+  # allow user override
+  arch <- getOption("reticulate.miniconda.arch")
+  if (!is.null(arch))
+    return(arch)
+  
+  # arm64 on macOS is not yet supported
+  info <- as.list(Sys.info())
+  if (info$machine == "i386")
+    return("x86")
+  
+  # otherwise, use arch as-is
+  info$machine
   
 }
 

commit cab569f218314b30c88dd64405dd691479269c8e

miniconda installer url uses x86_64 and not x86-64
This follows a regression after https://github.com/rstudio/reticulate/commit/9cf3178d344c6ed61e9648127038c020f1228a60

R/miniconda.R
@@ -121,6 +121,9 @@ miniconda_installer_arch <- function() {
   info <- as.list(Sys.info())
   if (info$machine == "i386")
     return("x86")
+  # miniconda url use x86_64 not x86-64 for Windows
+  if (info$machine == "x86-64")
+    return("x86_64")
   
   # otherwise, use arch as-is
   info$machine
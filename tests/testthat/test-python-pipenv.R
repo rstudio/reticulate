d15b9e95a3e51249fb23e34065cf90470111c1fd
Kevin Ushey <kevinushey@gmail.com>
2021-12-06 15:06:35 -0800

    add poetry test

--- tests/testthat/test-python-pipenv.R
+++ tests/testthat/test-python-pipenv.R
@@ -1,32 +1,32 @@
 context("pipenv")
 
 test_that("reticulate uses the pipenv-configured version of Python", {
-  
+
   if (!nzchar(Sys.which("pipenv")))
     skip("pipenv is not installed")
-  
+
   # move to temporary directory
   project <- tempfile("pipenv-")
   dir.create(project)
   on.exit(unlink(project), add = TRUE)
-  
+
   owd <- setwd(project)
   on.exit(setwd(owd), add = TRUE)
-  
+
   # initialize a pipenv project
   system("pipenv install", ignore.stdout = TRUE, ignore.stderr = TRUE)
-  
+
   # ask for virtualenv path
   expected <- system("pipenv --py", intern = TRUE)
-  
+
   # try running reticulate in child process
   fmt <- "R --vanilla -s -e '%s'"
   cmd <- sprintf(fmt, "writeLines(reticulate::py_config()$python)")
   actual <- system(cmd, intern = TRUE)
-  
+
   expect_equal(
     normalizePath(expected),
     normalizePath(actual)
   )
-  
+
 })
---
+++ tests/testthat/test-python-poetry.R
@@ -0,0 +1,42 @@
+context("poetry")
+
+test_that("reticulate uses the Poetry-configured version of Python", {
+
+  if (!nzchar(Sys.which("poetry")))
+    skip("poetry is not installed")
+
+  # move to temporary directory
+  project <- tempfile("poetry-")
+  dir.create(project)
+  on.exit(unlink(project), add = TRUE)
+
+  owd <- setwd(project)
+  on.exit(setwd(owd), add = TRUE)
+
+  # initialize project
+  system("poetry new .", ignore.stdout = TRUE, ignore.stderr = TRUE)
+  expect_true(file.exists("pyproject.toml"))
+
+  # remove dependency on pytest (poetry solver barfs?)
+  contents <- readLines("pyproject.toml")
+  contents <- grep("^pytest", contents, invert = TRUE, value = TRUE)
+  writeLines(contents, con = "pyproject.toml")
+
+  # try running python (force initialization of virtualenv)
+  system("poetry run python --version")
+
+  # ask for virtualenv path
+  envpath <- system("poetry env info --path", intern = TRUE)
+  expected <- virtualenv_python(envpath)
+
+  # try running reticulate in child process
+  fmt <- "R --vanilla -s -e '%s'"
+  cmd <- sprintf(fmt, "writeLines(reticulate::py_config()$python)")
+  actual <- system(cmd, intern = TRUE)
+
+  expect_equal(
+    normalizePath(expected),
+    normalizePath(actual)
+  )
+
+})

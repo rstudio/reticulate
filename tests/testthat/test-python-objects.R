00a747c40553b21f8c552f9bdf384a7793771e82
Tomasz Kalinowski <kalinowskit@gmail.com>
2022-01-13 15:29:38 -0500

    add tests for `py_len()` and `py_bool()`

--- tests/testthat/test-python-objects.R
+++ tests/testthat/test-python-objects.R
@@ -6,6 +6,16 @@ test_that("the length of a Python object can be computed", {
   m <- py_eval("[1, 2, 3]", convert = FALSE)
   expect_equal(length(m), 3L)
 
+  x <- py_eval("None", convert = FALSE)
+  expect_identical(length(x), 0L)
+  expect_identical(py_bool(x), FALSE)
+  expect_error(py_len(x), "'NoneType' has no len()")
+
+  x <- py_eval("object()", convert = FALSE)
+  expect_identical(length(x), 1L)
+  expect_identical(py_bool(x), TRUE)
+  expect_error(py_len(x), "'object' has no len()")
+
 })
 
 test_that("python objects with a __setitem__ method can be used", {

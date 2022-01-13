d7f2f3b328bf12e39a23977c8d0250c47cfd9611
Tomasz Kalinowski <kalinowskit@gmail.com>
2022-01-13 15:29:05 -0500

    silence warning about partial matching in test

--- tests/testthat/test-python-formals.R
+++ tests/testthat/test-python-formals.R
@@ -24,9 +24,9 @@ expect_formals <- function(given, expected) {
 }
 
 test_that("Python signatures convert properly", {
-  
+
   skip_if(py_version() < "3.3")
-  
+
   expect_formals('a', alist(a = ))
   expect_formals('a, b=1', alist(a = , b = NULL))
   expect_formals('a, *, b=1', alist(a = , ... = , b = NULL))
@@ -34,22 +34,22 @@ test_that("Python signatures convert properly", {
   expect_formals('a, b=1, **kw', alist(a = , b = NULL, ... = ))
   expect_formals('a, *args, **kw', alist(a = , ... = ))
   expect_formals('a, *args, b=1, **kw', alist(a = , ... = , b = NULL))
-  
+
 })
 
 test_that("Errors from e.g. builtins are not propagated", {
-  
+
   skip_if(py_version() < "3.3")
-  
+
   print <- import_builtins()$print
   expect_error(py_get_formals(print))
-  
+
 })
 
 test_that("The inspect.Parameter signature converts properly", {
-  
+
   skip_if(py_version() < "3.3")
-  
+
   # Parameter.empty usually signifies no default parameter,
   # but for args 3 and 4 here, it *is* the default parameter.
   inspect <- import("inspect", convert = TRUE)
@@ -59,20 +59,23 @@ test_that("The inspect.Parameter signature converts properly", {
     name = , kind = , ... = ,
     default = NULL, annotation = NULL
   ))
-  
+
 })
 
 test_that("Parameters are not matched by prefix", {
-  
+
   skip_if(py_version() < "3.3")
-  
+
   f_r <- function(long = NULL, ...) list(long, list(...))
   f_py <- py_eval('lambda long=None, **kw: (long, kw)')
   expect_identical(formals(f_r), py_get_formals(f_py))
+
+  op <- options(warnPartialMatchArgs = FALSE)
+  on.exit(options(op))
 
   # Normal R functions match partially:
   expect_identical(f_r(l = 2L), list(2L, list()))
   # Python functions behave as expected:
   expect_identical(f_py(l = 2L), list(NULL, list(l = 2L)))
-  
+
 })

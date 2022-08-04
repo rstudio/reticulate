190795c806abf260f654767134dad555d80f306a
Tomasz Kalinowski <kalinowskit@gmail.com>
2022-08-03 21:28:29 -0400

    skip plotly test w/ python2

--- tests/testthat/test-python-knitr-engine.R
+++ tests/testthat/test-python-knitr-engine.R
@@ -1,10 +1,12 @@
 context("knitr")
 
 test_that("An R Markdown document can be rendered using reticulate", {
-  
+
   skip_on_cran()
   skip_on_os("windows")
   skip_if_not_installed("rmarkdown")
+  skip_if(py_version() < "3") # plotly _repr_html_ test fails in py2
+
 
   modules <- c("numpy", "matplotlib", "pandas", "plotly")
   for (module in modules) {

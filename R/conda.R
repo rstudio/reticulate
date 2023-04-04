63e7b854e61bf6fdedfceb1579f2687a56dc18df
mkoohafkan <michael.koohafkan@gmail.com>
2023-04-03 21:37:04 -0700

    R interface to `conda search` conda_search()

--- R/conda.R
+++ R/conda.R
@@ -630,6 +630,59 @@ conda_python <- function(envname = NULL,
   path.expand(python)
 }
 
+#' @returns
+#'   `conda_search()` returns a data frame of search results for the specified
+#'   packages.
+#'
+#' @param matchspec A MatchSpec query string.
+#'
+#' @rdname conda-tools
+#' @export
+conda_search <- function(matchspec,
+                        forge = TRUE,
+                        channel = character(),
+                        conda = "auto",
+                        ...)
+{
+
+  conda <- conda_binary(conda)
+  local_conda_paths(conda)
+
+  args <- c("search", matchspec)
+
+  # add user-requested channels
+  channels <- if (length(channel))
+    channel
+  else if (forge)
+    "conda-forge"
+
+  for (ch in channels)
+    args <- c(args, "-c", ch)
+
+  args = c(args, "--json")
+  output <- system2(conda, shQuote(args), stdout = TRUE)
+  status <- attr(output, "status") %||% 0L
+  # check for errors
+  if (status != 0L) {
+    fmt <- "error searching for packages [error code %i]"
+    stopf(fmt, status)
+  }
+
+  parsed <- jsonlite::fromJSON(output)
+
+  # different channels can have different fields, so we need
+  # to pick the subset of fields common to all channels
+  common_cols = Reduce(intersect, lapply(parsed, names))
+  df = do.call(rbind, lapply(parsed, function(x) x[common_cols]))
+  rownames(df) = NULL
+
+  # return the basic fields, we could make it a user argument to
+  # return the full set of fields. Use intersect on the off chance
+  # the channel didn't include all of these basic fields
+  fields = intersect(c("name", "version", "build", "channel"), names(df))
+  df[fields]
+}
+
 
 find_conda <- function() {
 

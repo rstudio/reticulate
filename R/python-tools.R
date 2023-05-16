c092c49e6cc492e60633b1e1290c4e7db1aa50d5
Chun-Hui Gao <gaospecial@gmail.com>
2023-05-16 12:15:23 +0800

    Error in normalizePath(conda, winslash = "/", mustWork = TRUE)
    I don't know why but my conda history didn't have a `conda create` entry. So that the regex `script <- sub("^#\\s+cmd: (.+)\\s+create\\s+.*", "\\1",  lines[[1]])` failed to get the right path. I suggest to add a "install" in this regex to fix this issue as all need to install some modules in using conda.

--- R/python-tools.R
+++ R/python-tools.R
@@ -179,7 +179,7 @@ python_info_condaenv_find <- function(path) {
     return(NULL)
 
   # get path to conda script used
-  script <- sub("^#\\s+cmd: (.+)\\s+create\\s+.*", "\\1", lines[[1]])
+  script <- sub("^#\\s+cmd: (.+)\\s+(create|install)\\s+.*", "\\1", lines[[1]])
 
   # on Windows, a wrapper script is recorded in the history,
   # so instead attempt to find the real conda binary

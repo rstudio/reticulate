005ef6aedb9acab0a115321ce5818ea98f5237c0
Tomasz Kalinowski <tomasz@posit.co>
2023-09-11 14:17:26 -0400

    skip tests on CRAN
    New note on Debian win-builder CRAN test machine:
    
    ```
    * checking tests ... [8s/3s] NOTE
      Running ‘testthat.R’ [8s/3s]
    Running R code in ‘testthat.R’ had CPU time 2.7 times elapsed time
    ```

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,20 +1,26 @@
 
 run <- function() {
-  
+
+  on_cran <- !isTRUE(as.logical(Sys.getenv("NOT_CRAN", "false")))
+  if (on_cran) {
+    message("env var 'NOT_CRAN=true' not defined; Skipping tests on CRAN")
+    return()
+  }
+
   if (!requireNamespace("testthat", quietly = TRUE)) {
     message("'testthat' package not available; tests cannot be run")
     return()
   }
-  
+
   options(error = traceback)
   if (requireNamespace("rlang", quietly = TRUE))
     options(error = rlang::trace_back)
-  
+
   library(testthat)
   library(reticulate)
-  
+
   test_check("reticulate")
-  
+
 }
 
 run()

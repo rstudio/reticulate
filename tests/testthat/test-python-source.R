ade1e124bc35a596a04c94a85b0ca6a99d3ddfe5
Tomasz Kalinowski <tomasz@posit.co>
2024-08-11 19:31:15 -0700

    avoid using `skip_if_offline()`
    Would require taking a dependancy on {curl}.

--- tests/testthat/test-python-source.R
+++ tests/testthat/test-python-source.R
@@ -8,7 +8,7 @@ test_that("Python scripts can be sourced from local file", {
 
 test_that("Python scripts can be sourced from a URL", {
   skip_if_no_python()
-  skip_if_offline()
+  # skip_if_offline() ## needs {curl}
   source_python('https://raw.githubusercontent.com/rstudio/reticulate/main/tests/testthat/script.py')
   expect_equal(add(2, 4), 6)
 })

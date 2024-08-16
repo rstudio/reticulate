db6497a1ffacdf540ec7b7e1ce06f83026cf69b6
Tomasz Kalinowski <tomasz@posit.co>
2024-08-16 07:38:30 -0700

    add `py_run_file_on_thread()`

--- R/thread.R
+++ R/thread.R
@@ -17,3 +17,28 @@ py_main_thread_func <- function(f) {
   tools <- import("rpytools")
   tools$thread$main_thread_func(f)
 }
+
+
+py_allow_threads <- function(allow = TRUE) {
+  if (allow) {
+    reticulate_ns <- environment(sys.function())
+    for (f in sys.frames()) {
+      if (identical(parent.env(f), reticulate_ns) &&
+          !identical(f, environment()))
+        # Can't release the gil as unlocked while we're holding it
+        # elsewhere on the callstack.
+        stop("Python threads can only be unblocked from a top-level reticulate call")
+    }
+  }
+
+  if (!was_python_initialized_by_reticulate())
+    stop("Can't safely unblock threads when R is running embedded")
+
+  invisible(py_allow_threads_impl(allow))
+}
+
+
+
+py_run_file_on_thread <- function(file, ..., argv) {
+  import("rpytools.run")$`_launch_lsp_server_on_thread`(file, as.list(as.character(argv)))
+}

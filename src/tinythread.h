40b9b1a33e0e79b6aa7e43cb8891439db544ed56
Tomasz Kalinowski <kalinowskit@gmail.com>
2025-02-26 05:52:25 -0500

    Accomodate changing def of Rf_error()

--- src/tinythread.h
+++ src/tinythread.h
@@ -30,7 +30,7 @@ freely, subject to the following restrictions:
 #ifndef _TINYTHREAD_H_
 #define _TINYTHREAD_H_
 
-extern "C" void Rf_error(const char* fmt, ...);
+#include <R_ext/Error.h>  // for Rf_error()
 
 /// @file
 /// @mainpage TinyThread++ API Reference

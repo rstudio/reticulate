3776fc8e4811d0d8c526c356a35a014057af392c
Tomasz Kalinowski <kalinowskit@gmail.com>
2025-02-28 10:07:52 -0500

    document and export `py_register_load_hook`

--- R/python.R
+++ R/python.R
@@ -1431,7 +1431,7 @@ py_resolve_module_proxy <- function(proxy) {
 
   # fixup the proxy. Note, the proxy may have already been fixed up,
   # if `import(module)` triggered hooks to run registered via
-  # (unexported) py_register_load_hook()
+  # py_register_load_hook()
   py_module_proxy_import(proxy)
 
 
@@ -1585,11 +1585,29 @@ py_module_loaded <- function(module) {
   module %in% modules
 }
 
+
+#' Register a Python module load hook
+#'
+#' Register an R function to be called when a Python module is first loaded in
+#' the current R session. This can be used for tasks such as:
+#'
+#' - Delayed registration of S3 methods to accommodate different versions of a Python module.
+#' - Configuring module-specific logging streams.
+#'
+#' @param module String, the name of the Python module.
+#' @param hook Function, called with no arguments. If `module` is already
+#'   loaded, `hook()` is called immediately.
+#'
+#' @return `NULL` invisibly. Called for its side effect.
+#' @export
+#' @keywords internal
 py_register_load_hook <- function(module, hook) {
 
   # if the module is already loaded, just run the hook
-  if (py_module_loaded(module))
-    return(hook())
+  if (py_module_loaded(module)) {
+    hook()
+    return(invisible())
+  }
 
   # otherwise, register the hook to be run on next load
   name <- paste("reticulate", module, "load", sep = "::")
